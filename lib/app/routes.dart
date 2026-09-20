05423d5dc51458564e22dabd125491089aa8341e
joseph.gutierrez <hi@josephgutierrez.me>
2026-09-20 15:40:42 -0500

    feat(app): add named route constants

---
+++ lib/app/routes.dart
@@ -0,0 +1,11 @@
+class AppRoutes {
+  AppRoutes._();
+
+  static const String splash = '/';
+  static const String login = '/login';
+  static const String home = '/home';
+  static const String alarmEditor = '/alarm-editor';
+  static const String eventRule = '/event-rule';
+  static const String challengeSelector = '/challenge-selector';
+  static const String alarmRinging = '/alarm-ringing';
+}
